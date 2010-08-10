486ee9f128b4547107076f0f3671541b70797f4d
Timoteo Ponce <timo.slack@gmail.com>
2010-08-09 23:17:37 -0400

    - Initial model-objects description. - Complete unit test for Home component.

--- test/org/timo/personnel/model/dao/HomeTest.java
+++ test/org/timo/personnel/model/dao/HomeTest.java
@@ -6,11 +6,6 @@
 package org.timo.personnel.model.dao;
 
 import org.timo.personnel.model.User;
-import javax.persistence.EntityManager;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.timo.personnel.model.persistence.PersistenceUtil;
 import static org.junit.Assert.*;
@@ -21,139 +16,160 @@ import static org.junit.Assert.*;
  */
 public class HomeTest {
 
-    public HomeTest() {
-    }
-
-    /**
-     * Test of getId method, of class Home.
-     */
-//    @Test
-    public void testGetId() {
-        System.out.println("getId");
-        Home instance = null;
-        Object expResult = null;
-        Object result = instance.getId();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
     /**
      * Test of clearInstance method, of class Home.
      */
-//    @Test
+    @Test
     public void testClearInstance() {
         System.out.println("clearInstance");
-        Home instance = null;
-        instance.clearInstance();
-        fail("The test case is a prototype.");
+        final Home<User> home = (Home<User>) createHome();
+        home.getInstance().setUsername("test");
+
+        home.clearInstance();
+        assertNull(home.getInstance().getUsername());
     }
 
-    /**
-     * Test of setId method, of class Home.
-     */
-//    @Test
-    public void testSetId() {
-        System.out.println("setId");
-        Object id = null;
-        Home instance = null;
-        instance.setId(id);
-        fail("The test case is a prototype.");
-    }
 
     /**
      * Test of isIdDefined method, of class Home.
      */
-//    @Test
+    @Test
     public void testIsIdDefined() {
         System.out.println("isIdDefined");
-        Home instance = null;
-        boolean expResult = false;
-        boolean result = instance.isIdDefined();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+        final Home<User> home = (Home<User>) createHome();
+        assertFalse(home.isIdDefined());
+        home.setId(Integer.valueOf(25));
+        assertTrue(home.isIdDefined());
     }
 
     /**
      * Test of isManaged method, of class Home.
      */
-//    @Test
+    @Test
     public void testIsManaged() {
         System.out.println("isManaged");
-        Home instance = null;
-        boolean expResult = false;
-        boolean result = instance.isManaged();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+        final Home<User> home = (Home<User>) createHome();
+        home.getInstance().setUsername("test");
+        home.getInstance().setPassword("test");
+
+        assertFalse(home.isManaged());
+        home.persist();
+        assertTrue(home.isManaged());
     }
 
     /**
      * Test of update method, of class Home.
      */
-//    @Test
+    @Test
     public void testUpdate() {
         System.out.println("update");
-        Home instance = null;
-        instance.update();
-        fail("The test case is a prototype.");
+        final Home<User> home = (Home<User>) createHome();
+        home.getInstance().setUsername("test");
+        home.getInstance().setPassword("test");
+        home.persist();
+        final Integer savedId = home.getInstance().getId();
+
+        final String modString = "testModified";
+        home.getInstance().setUsername(modString);
+        home.update();
+
+        home.clearInstance();
+        home.setId(savedId);
+        assertEquals(modString, home.getInstance().getUsername());
     }
 
     /**
      * Test of persist method, of class Home.
      */
-//    @Test
+    @Test
     public void testPersist() {
         System.out.println("persist");
-        Home instance = null;
-        instance.persist();
-        fail("The test case is a prototype.");
+        final Home<User> home = (Home<User>) createHome();
+        home.getInstance().setUsername("test");
+        home.getInstance().setPassword("test");
+        home.persist();
+        final Integer savedId = home.getInstance().getId();
+        assertNotNull(savedId);
+
+        home.clearInstance();
+        home.setId(savedId);
+        assertEquals(savedId, home.getInstance().getId());
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testInitInstance() {
+        System.out.println("persist");
+        final Home<User> home = (Home<User>) createHome();
+        home.getInstance().setUsername("test");
+        home.getInstance().setPassword("test");
+        home.persist();
+        final Integer savedId = home.getInstance().getId();
+
+        home.clearInstance();
+        home.initInstance();
+        assertNull(home.getInstance().getId());
+
+        home.clearInstance();
+        home.setId(savedId);
+        home.initInstance();
+        assertEquals(savedId, home.getInstance().getId());
     }
 
     /**
      * Test of remove method, of class Home.
      */
-//    @Test
+    @Test
     public void testRemove() {
         System.out.println("remove");
-        Home instance = null;
-        instance.remove();
-        fail("The test case is a prototype.");
+        final Home<User> home = (Home<User>) createHome();
+        home.getInstance().setUsername("test");
+        home.getInstance().setPassword("test");
+        home.persist();
+
+        final Integer savedId = home.getInstance().getId();
+        home.clearInstance();
+        home.setId(savedId);
+        assertEquals(savedId, home.find().getId());
+
+        home.remove();
+        home.setId(savedId);
+        assertNull(home.find());
     }
 
     /**
      * Test of find method, of class Home.
      */
-    //@Test
+    @Test
     public void testFind() {
         System.out.println("find");
-        Home instance = null;
-        Object expResult = null;
-        Object result = instance.find();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
+        final Home<User> home = (Home<User>) createHome();
+        home.getInstance().setUsername("test");
+        home.getInstance().setPassword("test");
+        home.persist();
 
-    /**
-     * Test of initInstance method, of class Home.
-     */
-    //@Test
-    public void testInitInstance() {
-        System.out.println("initInstance");
-        Home instance = null;
-        instance.initInstance();
-        fail("The test case is a prototype.");
+        final Integer savedId = home.getInstance().getId();
+        home.clearInstance();
+        home.setId(savedId);
+        
+        assertEquals(savedId, home.find().getId());
     }
 
     /**
      * Test of createInstance method, of class Home.
      */
-    //@Test
+    @Test
     public void testCreateInstance() {
         System.out.println("createInstance");
-        Home instance = null;
-        Object expResult = null;
-        Object result = instance.createInstance();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+        final Home<User> home = (Home<User>) createHome();
+        home.getInstance().setId(25);
+
+        assertEquals(home.getInstance().getId(), Integer.valueOf(25));
+        
+        home.clearInstance();
+        assertNull(home.getInstance().getId());
     }
 
     /**
